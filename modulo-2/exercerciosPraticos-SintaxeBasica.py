5c52c62e8f1fc31f3ede47b7f4ef7ea7e7f1cec4
root <luizfelipedpt>
2026-05-26 17:52:23 -0300

    feat: sintaxe

---
+++ modulo-2/exercerciosPraticos-SintaxeBasica.py
@@ -0,0 +1,30 @@
+# Exercicio 1
+linguagem = "Python"
+print(linguagem)
+
+# Exercicio 2
+a = 1500
+b = 335
+print(a + b)
+print(a * b)
+print(a - b)
+
+# Exercicio 3
+a = "Marrie"
+b = " Currie "
+c = "!!!"
+print(a + b + c)
+
+# Exercicio 4
+print(2 + 3 == 5)
+
+# Exercicio 5
+print(5 + 9 == 13)
+
+# Exercicio 6
+pi = 3.1415 # Valor do número de pi
+print(pi)
+
+# Exercicio 7
+pi = 3.1415
+    print(pi) # Com um "tab" antes do print
